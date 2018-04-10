36dfc84666273730fc796d07e999987ede0ae325
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-04-09 22:05:17 -0300

    Step red in TDD

---
+++ tests/Mysql/SelectTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QueryBuilder\Mysql;
+
+class SelectTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSelectWithoutFilter()
+    {
+        $select = new Select;
+        $select->table('pages');
+
+        $actual = $select->getSql();
+        $expected = 'SELECT * FROM pages';
+
+        $this->assertEquals($expected, $actual);
+    }
+}
+
